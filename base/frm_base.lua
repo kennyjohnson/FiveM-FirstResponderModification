5c23e44eabdd0c7f894ec7eae92a17adb26218dc
Kenny Johnson <62081177+kennyjohnson@users.noreply.github.com>
2020-03-14 12:44:10 -0500

    Update frm_base.lua

--- base/frm_base.lua
+++ base/frm_base.lua
@@ -1,6 +1,27 @@
 frm = {}
 frm.config = {}
-frm.jobtable = {}
+frm.jobTable = {}
 frm.calltable = {}
 
-frm.status = true -- good
+function frm.error(text)
+  console.log("First Responder Modification ERROR")
+  console.log(text)
+end
+--[[
+frm.CreateJob( "24/7 Owner", 247_OWNER, {
+  description = [[]]
+  weapons = 
+})
+--]]
+function frm.CreateJob( ename, eteamid, fields )
+  local isvalid = true
+  for a=1, #frm.jobTable do
+    if eteamid == frm.jobTable[a].teamid then
+      frm.error("Attempting to create new job, found two TEAMIDs that match\n"..ename.." and "..frm.jobTable[a].name)
+      isvalid = false
+    end
+  end
+  if isvalid then
+      
+      jobTable[1 + #jobTable]={ name = ename, teamid = eteamid, desc = fields.desc, 
+    
