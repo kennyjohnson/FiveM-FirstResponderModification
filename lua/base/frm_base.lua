070c74a65654518e29b710b0879336ec6f2a6021
Kenny Johnson <62081177+kennyjohnson@users.noreply.github.com>
2020-03-14 15:10:05 -0500

    Create frm_base.lua

---
+++ lua/base/frm_base.lua
@@ -0,0 +1,4 @@
+FirstRepsonderMod = {}
+function FirstRepsonderMod.Initialize()
+  console.log("First Responder Modification Loaded Successfully\nCreated by kennyjohnson")
+  console.log("Support Discord https://discord.gg/vqYTneB")
