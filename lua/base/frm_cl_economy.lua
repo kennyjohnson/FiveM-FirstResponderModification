621df8f256394526122d3348956a27e9fce3b994
Kenny Johnson <62081177+kennyjohnson@users.noreply.github.com>
2020-03-17 16:03:24 -0500

    Delete frm_cl_economy.lua

--- lua/base/frm_cl_economy.lua
+++
@@ -1,8 +0,0 @@
-RegisterNetEvent("FirstResponderMod:updateClientMoney")
-AddEventHandler("FirstResponderMod:updateClientMoney", function(type, amount)
-    if type == "bank" then
-        StatSetInt('BANK_BALANCE', amount, true)
-    else
-        StatSetInt('MP0_WALLET_BALANCE', amount, true)
-    end
-end)
